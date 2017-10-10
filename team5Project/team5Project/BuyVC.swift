17aa6610a84adedd6796a8a2317c4f2bf2f0f813
nknguy <32646451+nknguy@users.noreply.github.com>
2017-10-10 17:00:59 -0500

    Add files via upload
    added buttons and label for some view controllers

---
+++ team5Project/team5Project/BuyVC.swift
@@ -0,0 +1,35 @@
+//
+//  BuyVC.swift
+//  team5Project
+//
+//  Created by Nguyen on 10/10/17.
+//  Copyright © 2017 Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class BuyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
