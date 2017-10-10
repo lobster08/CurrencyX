9fa80c176682f73024f742bf6e95b40544a2bd43
nknguy <32646451+nknguy@users.noreply.github.com>
2017-10-10 13:30:14 -0500

    Add files via upload
    create file project

---
+++ team5Project/team5Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  team5Project
+//
+//  Created by Nguyen on 10/10/17.
+//  Copyright © 2017 Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
